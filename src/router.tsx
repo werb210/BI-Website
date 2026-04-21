f5abed6a751adb5d47a6319b89c3c3fb8d8b5f32
werb210 <todd.w@boreal.financial>
2026-04-21 13:42:06 -0600

    Implement 3-step application flow with docs and signature

--- src/router.tsx
+++ src/router.tsx
@@ -3,10 +3,15 @@ import Intro from "./pages/Intro";
 import Quote from "./pages/Quote";
 import QuoteResult from "./pages/QuoteResult";
 import Application from "./pages/Application";
+import Documents from "./pages/Documents";
+import Signature from "./pages/Signature";
 
 export const router = createBrowserRouter([
   { path: "/", element: <Intro /> },
   { path: "/quote", element: <Quote /> },
   { path: "/quote/result", element: <QuoteResult /> },
+
   { path: "/apply", element: <Application /> },
+  { path: "/apply/documents", element: <Documents /> },
+  { path: "/apply/signature", element: <Signature /> },
 ]);
