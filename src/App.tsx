085f5dacaf4767965a64a5c5cbaf8f41db14b80b
werb210 <todd.w@boreal.financial>
2026-04-21 13:23:00 -0600

    fix: force redeploy after env hardening

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import LenderPortal from "./pages/LenderPortal"
 import ReferrerPortal from "./pages/ReferrerPortal"
 import Contact from "./pages/Contact"
 import MayaChat from "./components/MayaChat"
+import SubmitGuard from "./components/SubmitGuard"
 
 export default function App() {
 
@@ -15,10 +16,10 @@ export default function App() {
       <Routes>
 
         <Route path="/" element={<Home />} />
-        <Route path="/apply" element={<PGIApplication />} />
-        <Route path="/lender" element={<LenderPortal />} />
-        <Route path="/referrer" element={<ReferrerPortal />} />
-        <Route path="/contact" element={<Contact />} />
+        <Route path="/apply" element={<SubmitGuard><PGIApplication /></SubmitGuard>} />
+        <Route path="/lender" element={<SubmitGuard><LenderPortal /></SubmitGuard>} />
+        <Route path="/referrer" element={<SubmitGuard><ReferrerPortal /></SubmitGuard>} />
+        <Route path="/contact" element={<SubmitGuard><Contact /></SubmitGuard>} />
 
       </Routes>
       <MayaChat />
